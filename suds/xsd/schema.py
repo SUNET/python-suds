4b102784eededbfce01fdb3200c1448f480fe04e
Stefan Wold <swold@sunet.se>
2013-09-06 12:38:35 +0200

    Revert "applied https://fedorahosted.org/suds/attachment/ticket/239/recurselevel-schema.py.patch (recursion level patch)"
    This reverts commit 918c95bb3f53ade4e6ccf01214a8457b09976435.
    
    This patch incorrectly use global variables causing a second instantiation of a wsdl to fail.

--- suds/xsd/schema.py
+++ suds/xsd/schema.py
@@ -37,9 +37,6 @@ from logging import getLogger
 
 log = getLogger(__name__)
 
-PROCESSED_IMPORTS = []
-PROCESSED_IMPORT_DEPTH = []
-MAX_IMPORT_DEPTH = 3
 
 class SchemaCollection:
     """
@@ -397,21 +394,6 @@ class Schema:
         @rtype: L{Schema}
         @note: This is only used by Import children.
         """
-
-        global PROCESSED_IMPORTS, PROCESSED_IMPORT_DEPTH, MAX_IMPORT_DEPTH
-        if baseurl in PROCESSED_IMPORTS:
-            ind = PROCESSED_IMPORTS.index(baseurl)
-            if (PROCESSED_IMPORT_DEPTH[ind] < MAX_IMPORT_DEPTH):
-                PROCESSED_IMPORT_DEPTH[ind]+=1
-                log.debug('Increasing import count for: {0}'.format(baseurl))
-            else:
-                log.debug('Skipping processed import: {0}'.format(baseurl))
-                return None
-        else:
-            PROCESSED_IMPORTS.append(baseurl)
-            log.debug('Appending new import: {0}'.format(baseurl))
-            PROCESSED_IMPORT_DEPTH.append(1)
-
         return Schema(root, baseurl, options)
 
     def str(self, indent=0):
